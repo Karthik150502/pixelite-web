8d90898adc3cd70f9f5cf5177d2501565319987f
Karthik150502 <karthikrdycool@gmail.com>
2026-07-30 10:48:27 +0530

    Removed the black bg in the testimonials

--- components/animated/testimonials.tsx
+++ components/animated/testimonials.tsx
@@ -27,13 +27,13 @@ export const TestimonialsColumn = (props: {
                     ease: "linear",
                     repeatType: "loop",
                 }}
-                className="flex flex-col gap-6 pb-6 bg-background"
+                className="flex flex-col gap-6 pb-6 bg-transparent"
             >
                 {[
                     ...new Array(2).fill(0).map((_, index) => (
                         <React.Fragment key={index}>
                             {props.testimonials.map(({ text, image, name, role }, i) => (
-                                <div className="p-10 rounded-3xl border border-white/30 bg-white/10 backdrop-blur-xl shadow-lg shadow-primary/10 max-w-xs w-full dark:border-white/10 dark:bg-black/10" key={i}>
+                                <div className="p-10 rounded-3xl border border-white/30 shadow-lg shadow-primary/10 max-w-xs w-full dark:border-white/10 bg-transparent" key={i}>
                                     <div className="text-zinc-600 dark:text-zinc-400 text-sm font-light">{text}</div>
                                     <div className="flex items-center gap-2 mt-5">
                                         <Image
