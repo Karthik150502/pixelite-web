98afe73b8430f3f774ef6ec42e4fa1453502d8a5
Karthik150502 <karthikrdycool@gmail.com>
2026-08-01 23:44:42 +0530

    About change

--- components/animated/about-hero-demo.tsx
+++ components/animated/about-hero-demo.tsx
@@ -1,15 +1,19 @@
 import { MinimalistHero } from './about-hero';
-import { socials } from '@/lib/constants/metadata';
+import { cal_dot_com_url, socials } from '@/lib/constants/metadata';
 
 const MinimalistHeroDemo = () => {
     return (
         <MinimalistHero
-            mainText="Hi, I am the person you are looking at right now. Pixelite Studio was an idea of two best friends who are fan extraordinary."
+            mainText="Pixelite Studio is an idea of two creative minds in the world of photography who never settle for nothing less than extraordinary"
             imageSrc="https://dl.dropboxusercontent.com/scl/fi/udqk30goqo2mun8oy05wr/IMG_4855.PNG?rlkey=xwk7bsk19f18z8n003ncay399&st=xwcu44az&dl=0"
             imageAlt="A portrait of a person in a brown shirt, in profile."
             overlayText={{
                 part1: 'I am',
                 part2: 'Karthik',
+            }}
+            contact={{
+                contactButtonText: "talk to me",
+                contactUrl: cal_dot_com_url
             }}
             socialLinks={socials['Karthik J']}
             locationText="Bangalore, Karnataka, India"
